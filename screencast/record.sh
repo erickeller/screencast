d6ed63d207ae10110c75825ce456afd8b3a5c2dd
Eric Keller <eric.keller.ek1@roche.com>
2014-02-19 07:25:18 +0100

    first argument to the script defines the video name

--- screencast/record.sh
+++ screencast/record.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 RECORDING_DELAY=5
-OUTPUT=test
+if [ $# -eq 1 ]
+then
+  OUTPUT=${1}
+else
+  OUTPUT=test
+fi
 
 recordmydesktop --windowid `xwininfo | awk '/Window id:/ {print $4}'` -o ${OUTPUT}.ogv --delay ${RECORDING_DELAY} --no-sound &
 echo "stop recording:
