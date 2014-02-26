2497a3aaf2863b54109034aa41c5b99fe51e5b70
Eric Keller <eric.keller.ek1@roche.com>
2014-02-26 09:02:45 +0100

    add text to video convertion using avconv

--- txt2video/txt2video.sh
+++ txt2video/txt2video.sh
@@ -17,13 +17,16 @@ fi
 
 OUTPUT_TEXT=$1
 #OUTPUT_IMG=$(awk -F: '$0=$1' <<<"${OUTPUT_TEXT}")
-OUTPUT_IMG="`echo ${OUTPUT_TEXT} | cut -c 1-6 | tr ' ' '_'`.png"
+OUTPUT_NAME=`echo ${OUTPUT_TEXT} | cut -c 1-6 | tr ' ' '_'`
+OUTPUT_IMG="${OUTPUT_NAME}.png"
+OUTPUT_VIDEO="${OUTPUT_NAME}.avi"
 VIDEO_LENGTH=$2
+RATIO=1280x720
 
-echo "generating ${VIDEO_LENGTH} s video from ${OUTPUT_TEXT}"
 echo "generating image: ${OUTPUT_IMG} from text: ${OUTPUT_TEXT}"
-convert -size 1280x720 xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center caption:"${OUTPUT_TEXT}" -flatten ${OUTPUT_IMG}
-
+convert -size ${RATIO} xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center caption:"${OUTPUT_TEXT}" -flatten ${OUTPUT_IMG}
+echo "generating ${VIDEO_LENGTH} s video from ${OUTPUT_TEXT} with ${OUTPUT_IMG}"
+avconv -f image2 -i ${OUTPUT_IMG} -r "1/${VIDEO_LENGTH}" -s "${RATIO}" ${OUTPUT_VIDEO} -t "00:00:${VIDEO_LENGTH}"
 exit 0
 
 #convert -size 1280x720 xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center -draw "text 0,0 'Linux\nand\nLife'" linuxandlife.png
