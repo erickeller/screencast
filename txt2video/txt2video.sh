a5a65dafc7c08876f90fb78909a9407b60d0e2d9
Eric Keller <eric.keller.ek1@roche.com>
2014-02-26 07:53:49 +0100

    add imagemagick convert function to create image from text

--- txt2video/txt2video.sh
+++ txt2video/txt2video.sh
@@ -16,8 +16,14 @@ then
 fi
 
 OUTPUT_TEXT=$1
+#OUTPUT_IMG=$(awk -F: '$0=$1' <<<"${OUTPUT_TEXT}")
+OUTPUT_IMG="`echo ${OUTPUT_TEXT} | cut -c 1-6 | tr ' ' '_'`.png"
 VIDEO_LENGTH=$2
 
 echo "generating ${VIDEO_LENGTH} s video from ${OUTPUT_TEXT}"
+echo "generating image: ${OUTPUT_IMG} from text: ${OUTPUT_TEXT}"
+convert -size 1280x720 xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center caption:"${OUTPUT_TEXT}" -flatten ${OUTPUT_IMG}
 
 exit 0
+
+#convert -size 1280x720 xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center -draw "text 0,0 'Linux\nand\nLife'" linuxandlife.png
