96be10d80c7d3a3aeed912766dc7d3720bfbf36e
Eric Keller <eric.keller.ek1@roche.com>
2014-02-28 22:31:27 +0100

    add some composition, to watermark an image with a logo

--- txt2video/txt2video.sh
+++ txt2video/txt2video.sh
@@ -20,6 +20,8 @@ OUTPUT_TEXT=$1
 #OUTPUT_IMG=$(awk -F: '$0=$1' <<<"${OUTPUT_TEXT}")
 OUTPUT_NAME=`echo ${OUTPUT_TEXT} | cut -c 1-6 | tr ' ' '_'`
 OUTPUT_IMG="${OUTPUT_NAME}.png"
+#LOGO_IMG=logo.png
+#WATERMARK_IMG="${OUTPUT_NAME}.watermarked.png"
 if [ $# -eq 3 ]
 then
   OUTPUT_VIDEO=${3}.avi
@@ -31,8 +33,8 @@ RATIO=1280x720
 
 echo "generating image: ${OUTPUT_IMG} from text: ${OUTPUT_TEXT}"
 convert -size ${RATIO} xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center caption:"${OUTPUT_TEXT}" -flatten ${OUTPUT_IMG}
+echo "Watermark ${OUTPUT_IMG} image with ${LOGO_IMG} logo"
+#composite -gravity NorthEast ${LOGO_IMG} ${OUTPUT_IMG} ${WATERMARK_IMG}
 echo "generating ${VIDEO_LENGTH} s video from ${OUTPUT_TEXT} with ${OUTPUT_IMG}"
 avconv -f image2 -i ${OUTPUT_IMG} -r "1/${VIDEO_LENGTH}" -s "${RATIO}" ${OUTPUT_VIDEO} -t "00:00:${VIDEO_LENGTH}"
 exit 0
-
-#convert -size 1280x720 xc:white -font FreeMono-Regular -pointsize 72 -fill black  -gravity center -draw "text 0,0 'Linux\nand\nLife'" linuxandlife.png
